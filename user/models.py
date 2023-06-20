e2f5e228a17634e96685c526e008db8613830430
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-20 11:28:50 +0300

    upd User with new image-field: "avatar"

--- user/models.py
+++ user/models.py
@@ -1,8 +1,12 @@
+import os
+import uuid
+
 from django.contrib.auth.models import (
     AbstractUser,
     BaseUserManager,
 )
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext as _
 
 
@@ -40,6 +44,13 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/avatar/", filename)
+
+
 class User(AbstractUser):
     class GenderChoices(models.TextChoices):
         MALE = "male"
@@ -53,6 +64,7 @@ class User(AbstractUser):
     followers = models.ManyToManyField(
         to="self", blank=True, related_name="following", symmetrical=False
     )
+    avatar = models.ImageField(null=True, blank=True, upload_to=movie_image_file_path)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
