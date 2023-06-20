1a9395b20092af002409e3e27caf402ed7e2e479
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-20 13:18:26 +0300

    add logic when need upload image & see image

--- user/models.py
+++ user/models.py
@@ -46,7 +46,7 @@ class UserManager(BaseUserManager):
 
 def movie_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.pk)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/avatar/", filename)
 
--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
@@ -16,3 +18,20 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific movie"""
+        image = self.get_object()
+        serializer = self.get_serializer(image, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
