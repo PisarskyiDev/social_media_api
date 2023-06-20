bbedeb9b09c7cd862fe0332140ff1ef3beee34de
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-20 15:46:46 +0300

    add serializer to create new profile (anyone can do it)

--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff", "avatar")
+        fields = ("id", "email", "password", "is_staff", "followers", "avatar")
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
@@ -22,3 +22,11 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class UserCreateSerializer(UserSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("email", "password", "avatar")
+        read_only_fields = ("is_staff",)
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
