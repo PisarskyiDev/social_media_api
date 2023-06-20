db2d68735cd0f57a9be34700b1923ab7d838e546
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-20 21:11:07 +0300

    fix password-field, set required=False

--- user/serializers.py
+++ user/serializers.py
@@ -18,7 +18,9 @@ class UserSerializer(serializers.ModelSerializer):
             "avatar",
         )
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {
+            "password": {"write_only": True, "min_length": 5, "required": False}
+        }
 
     @staticmethod
     def get_followers(obj):
@@ -53,4 +55,6 @@ class UserCreateSerializer(UserSerializer):
         model = get_user_model()
         fields = ("email", "password", "sex", "avatar")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {
+            "password": {"write_only": True, "min_length": 5, "required": False}
+        }
