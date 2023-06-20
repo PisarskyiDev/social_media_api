2a788fe2e4fbe3d65ef092a0b03fb83e1e21ce64
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-20 15:58:44 +0300

    upd RetrieveUpdateAPIView to GenericViewSet & using mixins

--- user/views.py
+++ user/views.py
@@ -1,37 +1,20 @@
-from rest_framework import generics, status
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics, viewsets, mixins
 
-from user.serializers import UserSerializer
+from user.models import User
+from user.serializers import UserSerializer, UserCreateSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    authentication_classes = ()
+    permission_classes = ()
+
+
+class UserView(
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class ManageUserView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserSerializer
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def get_object(self):
-        return self.request.user
-
-    @action(
-        methods=["POST"],
-        detail=True,
-        url_path="upload-image",
-        permission_classes=[IsAdminUser],
-    )
-    def upload_image(self, request, pk=None):
-        """Endpoint for uploading image to specific movie"""
-        image = self.get_object()
-        serializer = self.get_serializer(image, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
