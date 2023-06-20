cfce93aab0dfb6e5a242da48b23fea12aa966ea2
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-20 22:59:00 +0300

    add PostAdmin && del Group from admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "created_at",
+        "updated_at",
+        "owner",
+    )
+    search_fields = (
+        "title",
+        "owner__email",
+    )
+    ordering = ("-created_at",)
+
+
+admin.site.unregister(Group)
