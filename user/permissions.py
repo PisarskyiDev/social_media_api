8677982c3edbf59a388b8b238a22bd3431c15409
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-21 15:10:41 +0300

    add IsOwnerOrAdminOrReadOnly (custom-permission)

--- user/permissions.py
+++ user/permissions.py
@@ -11,3 +11,17 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
             )
             or (request.user and request.user.is_staff)
         )
+
+
+class IsOwnerOrAdminOrReadOnly(BasePermission):
+    """
+    Custom permission to only allow owners or admins to edit it
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner or admin.
+        return obj.owner == request.user or request.user.is_staff
