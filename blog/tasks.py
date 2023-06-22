446f728c339adb012579150c34a80510bcc3f442
soobig666 <darkvader.ukr.net@gmail.com>
2023-06-22 14:16:52 +0300

    add func generate_random_post to generate absolutely random Post-model, using Celery & Django-Celery-beat & Redis

--- blog/tasks.py
+++ blog/tasks.py
@@ -1,11 +1,26 @@
-# Create your tasks here
-
-# from blog.models import
-# TODO: add usable model
-
+import random
+from blog.models import Post
+from user.models import User
 from celery import shared_task
 
 
 @shared_task
-def generate_new_post():
-    return 5 + 5
+def generate_random_post():
+    words = [
+        "Lorem",
+        "Ipsum",
+        "Dolor",
+        "Sit",
+        "Amet",
+        "Consectetur",
+        "Adipiscing",
+        "Elit",
+    ]
+
+    title = " ".join(random.choices(words, k=5))
+
+    content = " ".join(random.choices(words, k=50))
+
+    owner = User.objects.order_by("?").first()
+
+    Post.objects.create(title=title, content=content, owner=owner)
